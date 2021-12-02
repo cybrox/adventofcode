cfff5fa8d5573167aeba93ec47dd4aecefbd95fa
Sven Gehring <cbrxde@gmail.com>
2021-12-02 15:49:16 +0100

    2015-04

---
+++ 2015/day_4/4a.ex
@@ -0,0 +1,15 @@
+input =
+  "./4.input.txt"
+  |> File.read!()
+  |> String.trim()
+
+defmodule Solution do
+  def iter_for(seed), do: iter_for(seed, 0)
+
+  def iter_for(seed, n) do
+    <<l::binary-size(5), _::binary>> = :crypto.hash(:md5, seed <> "#{n}") |> Base.encode16()
+    if l == "00000", do: n, else: iter_for(seed, n + 1)
+  end
+end
+
+IO.puts("Found hash with 5 leading zeroes at iteration #{Solution.iter_for(input)}")
