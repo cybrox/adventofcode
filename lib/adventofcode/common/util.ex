7809d87f8ad6f48d656894e341600b9cfb164907
Sven Gehring <cbrxde@gmail.com>
2023-12-19 12:41:01 +0100

    feat: fun with ranges in utils

--- lib/adventofcode/common/util.ex
+++ lib/adventofcode/common/util.ex
@@ -14,4 +14,49 @@ defmodule AdventOfCode.Common.Util do
   These two steps occur in a lot of puzzles
   """
   def str2int(str, trim \\ " "), do: str |> String.trim(trim) |> String.to_integer()
+
+  @doc """
+  Given two ranges A and B, get back a sub-range of range A with all the values that
+  are also contained within range B. In other words, get the intersection of range A and B.
+  """
+  def intersect_onto(nil, _), do: nil
+  def intersect_onto(_, nil), do: nil
+
+  def intersect_onto(sa..ea = range_a, sb..eb = range_b) do
+    cond do
+      # match range is fully outside original range
+      Range.disjoint?(range_a, range_b) -> nil
+      # match range is fully inside original range
+      sb > sa and eb < ea -> range_b
+      # match range fully envelops original range
+      sb <= sa and eb >= ea -> range_a
+      # match range clips start of original range
+      sb < sa and eb >= sb -> sa..eb
+      # match range clips end of original range
+      sb <= ea and eb > ea -> sb..ea
+    end
+  end
+
+  @doc """
+  Given two ranges A and B, get back a sub-range (or list of sub-ranges) of range A with all the
+  values that are not also contained within range B. In other words, get the complement of range
+  A with respect to range B.
+  """
+  def complement_onto(nil, _), do: nil
+  def complement_onto(range_a, nil), do: range_a
+
+  def complement_onto(sa..ea = range_a, sb..eb = range_b) do
+    cond do
+      # match range is fully outside original range
+      Range.disjoint?(range_a, range_b) -> range_a
+      # match range is fully inside original range
+      sb > sa and eb < ea -> [sa..(sb - 1), (eb + 1)..ea]
+      # match range fully envelops original range
+      sb <= sa and eb >= ea -> nil
+      # match range clips start of original range
+      sb < sa and eb >= sb -> (eb + 1)..ea
+      # match range clips end of original range
+      sb <= ea and eb > ea -> sa..(sb - 1)
+    end
+  end
 end
--- test/common/util_test.exs
+++ test/common/util_test.exs
@@ -29,4 +29,68 @@ defmodule AdventOfCode.Common.UtilTest do
       assert Testee.str2int("78,", ",") == 78
     end
   end
+
+  describe "intersect_onto/2" do
+    test "returns correct value for different range constellations" do
+      # Range B is fully outside of range A
+      assert Testee.intersect_onto(20..50, 0..10) == nil
+      assert Testee.intersect_onto(20..50, 60..100) == nil
+
+      # Range B fully encapsulates range A
+      assert Testee.intersect_onto(20..50, 0..100) == 20..50
+      assert Testee.intersect_onto(20..50, 20..50) == 20..50
+
+      # Range A fully encapsulates range B
+      assert Testee.intersect_onto(20..50, 30..40) == 30..40
+      assert Testee.intersect_onto(20..50, 35..45) == 35..45
+
+      # Range B clips range A at the beginning
+      assert Testee.intersect_onto(20..50, 0..20) == 20..20
+      assert Testee.intersect_onto(20..50, 10..20) == 20..20
+
+      # Range B intersects range A at the beginning
+      assert Testee.intersect_onto(20..50, 0..30) == 20..30
+      assert Testee.intersect_onto(20..50, 10..40) == 20..40
+
+      # Range B clips range A at the end
+      assert Testee.intersect_onto(20..50, 50..70) == 50..50
+      assert Testee.intersect_onto(20..50, 50..100) == 50..50
+
+      # Range B intersects range A at the end
+      assert Testee.intersect_onto(20..50, 30..60) == 30..50
+      assert Testee.intersect_onto(20..50, 40..100) == 40..50
+    end
+  end
+
+  describe "complement_onto/2" do
+    test "returns correct value for different range constellations" do
+      # Range B is fully outside of range A
+      assert Testee.complement_onto(20..50, 0..10) == 20..50
+      assert Testee.complement_onto(20..50, 60..100) == 20..50
+
+      # Range B fully encapsulates range A
+      assert Testee.complement_onto(20..50, 0..100) == nil
+      assert Testee.complement_onto(20..50, 20..50) == nil
+
+      # Range A fully encapsulates range B
+      assert Testee.complement_onto(20..50, 30..40) == [20..29, 41..50]
+      assert Testee.complement_onto(20..50, 35..45) == [20..34, 46..50]
+
+      # Range B clips range A at the beginning
+      assert Testee.complement_onto(20..50, 0..20) == 21..50
+      assert Testee.complement_onto(20..50, 10..20) == 21..50
+
+      # Range B intersects range A at the beginning
+      assert Testee.complement_onto(20..50, 0..30) == 31..50
+      assert Testee.complement_onto(20..50, 10..40) == 41..50
+
+      # Range B clips range A at the end
+      assert Testee.complement_onto(20..50, 50..70) == 20..49
+      assert Testee.complement_onto(20..50, 50..100) == 20..49
+
+      # Range B intersects range A at the end
+      assert Testee.complement_onto(20..50, 30..60) == 20..29
+      assert Testee.complement_onto(20..50, 40..100) == 20..39
+    end
+  end
 end
